907631008683048fcf915bd3a945b1aeddacb540
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-05-19 16:30:38 -0600

    2020-05-19 16:30, latest updates, using Python to commit

--- backend/herokuGorillaServer.py
+++ backend/herokuGorillaServer.py
@@ -10,45 +10,49 @@ def setupWaitressServer(flaskApp):
 	flaskApp.run()
 
 
+def setupFlaskServer(flaskApp):
+
+	flaskApp.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
+	@flaskApp.route('/datarequests', methods=['GET', 'POST'])
+	def datarequests():
+
+		if request.method == 'GET':
+			dataToSendToFrontend = {
+				'cat eyes': 'yellow',
+				'collar': 'red'
+			}
+
+			return Response(json.dumps(dataToSendToFrontend), mimetype='application/json')
+
+
+		if request.method == 'POST':
+			requestObj = request.json
+
+			if requestObj['spreadsheetType'] == 'public':
+				return render_template('reconcilePublic.html')
+			else:
+				return render_template('reconcilePrivate.html')
+
+
+	@flaskApp.route('/')
+	def returnMainPage():
+		return render_template('index.html')
+		# return """	<p>Spreadsheet to reconcile:</p>
+		# 			<button onclick="publicClickFunction()">Public</button>
+		# 			<button onclick="privateClickFunction()">Private</button>
+		# 			<p></p>
+		# 			<img src="./frontend/assets/regal-cat.jpeg" alt="regal cat" />"""
+
+
+	if __name__ == '__main__':
+		
+		# waitress.serve(flaskApp, host='0.0.0.0', port=8000)
+		# flaskApp.run()
+		setupWaitressServer(flaskApp)
+
 
 
 flaskApp = Flask(__name__, static_folder='../frontend/', template_folder='../frontend/htmlTemplates')
-flaskApp.config['TEMPLATES_AUTO_RELOAD'] = True
-
-
-@flaskApp.route('/datarequests', methods=['GET', 'POST'])
-def datarequests():
-
-	if request.method == 'GET':
-		dataToSendToFrontend = {
-			'cat eyes': 'yellow',
-			'collar': 'red'
-		}
-
-		return Response(json.dumps(dataToSendToFrontend), mimetype='application/json')
-
-
-	if request.method == 'POST':
-		requestObj = request.json
-
-		if requestObj['spreadsheetType'] == 'public':
-			return render_template('reconcilePublic.html')
-		else:
-			return render_template('reconcilePrivate.html')
-
-
-@flaskApp.route('/')
-def returnMainPage():
-	return render_template('index.html')
-	# return """	<p>Spreadsheet to reconcile:</p>
-	# 			<button onclick="publicClickFunction()">Public</button>
-	# 			<button onclick="privateClickFunction()">Private</button>
-	# 			<p></p>
-	# 			<img src="./frontend/assets/regal-cat.jpeg" alt="regal cat" />"""
-
-
-if __name__ == '__main__':
-	
-	# waitress.serve(flaskApp, host='0.0.0.0', port=8000)
-	# flaskApp.run()
-	setupWaitressServer(flaskApp)
+setupFlaskServer(flaskApp)
