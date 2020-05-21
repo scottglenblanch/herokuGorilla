6e41665db9b465878cba78ea0e2bcb7ed7f0fe6e
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-05-21 17:38:57 -0600

    2020-05-21 17:38, latest updates, using Python to commit

--- backend/herokuGorillaServer.py
+++ backend/herokuGorillaServer.py
@@ -21,7 +21,7 @@ def setupFlaskServer(flaskApp):
 
 		if request.method == 'GET':
 			dataToSendToFrontend = {
-				'otherInfo': str(flaskApp.config['SERVER_NAME']),
+				'otherInfo': str(flaskApp.config),
 				'cat eyes': 'yellow',
 				'collar': 'red'
 			}
